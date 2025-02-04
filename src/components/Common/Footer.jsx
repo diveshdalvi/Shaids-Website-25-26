b23faae73c9e9384af234e7a42b46ca54ecb172b
bhargavgajare1479 <gajarebhargav@gmail.com>
2025-02-04 21:55:41 +0530

    Rework Footer

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -2,29 +2,32 @@ import { Instagram, GitHub, LinkedIn } from "@mui/icons-material";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-[#2E0B52] text-white text-center"> {/* Outer div for full width */}
-      <div className="px-4 max-w-16xl mx-auto flex flex-col md:flex-row justify-between items-center">
+    <div className="w-full text-white">
+      <hr className="border-t border-white opacity-50 mb-8"/>
+      {/* Main Container */}
+      <div className="max-w-7xl mx-auto px-4 py-12 flex flex-col md:flex-row justify-between items-start md:items-center gap-8 md:gap-96">
+        
         {/* Left Section */}
-        <div className="text-left px-4 py-4">
+        <div className="flex flex-col items-start w-full md:w-1/2">
           <div className="flex items-center space-x-3">
-            <img src="./src/assets/images/logo.svg" alt="SHAIDS Logo" className="h-20 w-20"/>
-            <h2 className="text-xl font-semibold">SHAIDS.</h2>
+            <img src="./src/assets/images/logo.svg" alt="SHAIDS Logo" className="h-16 w-16"/>
+            <h2 className="text-2xl font-semibold">SHAIDS.</h2>
           </div>
-          <p className="mt-2 mr-0 sm:mr-8 text-base font-Outfit ">
-            Empowering DMCE in AI & Data Science<br />through innovation.<br />
+          <p className="mt-2 text-base font-Outfit">
+            Empowering DMCE in AI & Data Science through innovation.<br />
             Join us in learning and collaboration.
           </p>
         </div>
 
         {/* Right Section */}
-        <div className="text-base font-lg text-left mt-8 md:mt-16 px-8 font-Outfit">
-          <h3 className="font-Outfit">DATTA MEGHE COLLEGE OF ENGINEERING</h3>
-          <p>
+        <div className="text-left w-full md:w-1/2">
+          <h3 className="text-lg font-semibold">DATTA MEGHE COLLEGE OF ENGINEERING</h3>
+          <p className="text-base">
             SEC-3, AIROLI, NAVI MUMBAI<br />
             MAHARASHTRA - 400708, INDIA
           </p>
           {/* Social Icons */}
-          <div className="flex space-x-4 mt-3 justify-start">
+          <div className="flex space-x-4 mt-3">
             <a href="https://www.instagram.com/dmceairoli.official/" className="text-pink-400 hover:text-pink-500 text-xl">
               <Instagram />
             </a>
@@ -39,8 +42,8 @@ const Footer = () => {
       </div>
       
       {/* Bottom Section */}
-      <div className="bg-[#552D91] mt-16 py-2 max-w-full">
-        <p className="text-xs font-Outfit text-start px-4">© 2025 DMCE SHAIDS. All rights reserved.</p>
+      <div className="py-2 text-center">
+        <p className="text-xs font-Outfit">© 2025 DMCE SHAIDS. All rights reserved.</p>
       </div>
     </div>
   );
