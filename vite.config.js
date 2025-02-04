682e442533a49bc4cb27638f4cc269581ef54b89
Nachiket Patil <166910327+NachiketPatil26@users.noreply.github.com>
2025-02-05 01:37:34 +0530

    Updated base repo name

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Shaids-Website-25-26",
+  base:"/Shaids-Website-25-26/",
   
 })
