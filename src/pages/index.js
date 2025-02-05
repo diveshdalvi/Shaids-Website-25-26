7362079e78d85ac39efb748335957f5066268172
Ayush Bhosale <127243663+AyushBhosale@users.noreply.github.com>
2025-02-05 12:04:10 +0530

    Updates

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Achievements } from "./Achievements.jsx";
-export { default as ContactUs } from "./ContactUs.jsx";
-export { default as Team } from "./Team.jsx";
-export { default as Events } from "./Events.jsx";
